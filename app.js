60f7f45ebf45d85a0e42ebbfd0a3ac088e90d077
s-sucharita <sucharita5991@gmail.com>
2026-01-21 22:54:29 +0530

    installed the connections

---
+++ app.js
@@ -0,0 +1,16 @@
+require ("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});app.use(cors());
+
