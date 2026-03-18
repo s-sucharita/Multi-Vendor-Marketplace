e25258b337132af7f63882767131d42aea42640f
s-sucharita <sucharita5991@gmail.com>
2026-03-18 12:25:26 +0530

    Edited package.jsom

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://multi-vendor-marketplace-khaa.onrender.com/api",
 });
 
 // Attach token automatically
